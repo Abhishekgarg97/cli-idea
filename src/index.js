5246417dcaf188901bd0cd31f82f171ef5d9cedb
Abhishekgarg97 <59972803+Abhishekgarg97@users.noreply.github.com>
2021-06-23 21:55:24 +0530

    v1.1.0

--- src/index.js
+++ src/index.js
@@ -1,11 +1,14 @@
-const fs = require("fs");
-const path = require("path");
-const inquirer = require("inquirer");
-const nodeExpress = require("./configs/nodeExpress");
-const staticConfig = require("./configs/staticConfig");
-const fef = require("./configs/fef");
+#! /usr/bin/env node
+const fs = require('fs');
+const path = require('path');
+const inquirer = require('inquirer');
+const nodeExpress = require('./configs/nodeExpress');
+const staticConfig = require('./configs/staticConfig');
+const fef = require('./configs/fef');
 
-const existingconfig = fs.existsSync("now.json");
+const nowPath = path.join(process.cwd(), 'now.json');
+
+const existingconfig = fs.existsSync('now.json');
 
 async function buildconfig() {
   let config = {
@@ -13,57 +16,78 @@ async function buildconfig() {
   };
   const answers = await inquirer.prompt([
     {
-      type: "text",
-      name: "name",
-      message: "what is the name of project?",
+      type: 'text',
+      name: 'name',
+      message: 'what is the name of project?',
       default: path.basename(process.cwd()),
     },
     {
-      type: "list",
-      name: "type",
-      message: "what type of project?",
+      type: 'list',
+      name: 'type',
+      message: 'what type of project?',
       choices: [
-        "node-express",
-        "static",
-        "react",
-        "vue",
-        "static-build",
-        "lambda",
+        'node-express',
+        'static',
+        'react',
+        'vue',
+        'static-build',
+        'lambda',
       ],
     },
   ]);
   config.name = answers.name;
   switch (answers.type) {
-    case "node-express":
+    case 'node-express':
       // eslint-disable-next-line no-undef
       config = await nodeExpress(config);
       break;
-    case "staticConfig":
+    case 'staticConfig':
       // eslint-disable-next-line no-undef
       config = await staticConfig(config);
       break;
-    case "react":
-      config = await fef(config, "build");
+    case 'react':
+      config = await fef(config, 'build');
       break;
-    case "vue":
+    case 'vue':
       config = await fef(config);
       break;
-    case "static-build":
+    case 'static-build':
       config = await fef(config);
       break;
     default:
       break;
   }
-  console.log(config);
+  const moreAnswers = await inquirer
+    .prompt([
+      {
+        type: 'confirm',
+        name: 'specifyAlias',
+        message: 'Would you like to specify an alias? 🤖',
+        default: true,
+      },
+      {
+        type: 'text',
+        name: 'alias',
+        message: 'What is the alias? 👤\n(Specify multiple separated by commas.)',
+        default: answers.name,
+        when: (a) => a.specifyAlias,
+      },
+    ]);
+  config.alias = moreAnswers.alias ? moreAnswers.alias.split(',').map((a) => a.trim()) : undefined;
+  fs.writeFileSync(nowPath, JSON.stringify(config, null, 2), 'utf8');
+  console.log('All done! 🎉 Type now to deploy! 🚀');
+  process.exit(0);
 }
+
+
 const questions = [];
 if (existingconfig) {
   inquirer
     .prompt([
       {
-        type: "confirm",
-        name: "Overwrite",
-        message: "now.json already exists! Would you like to Overwrite?",
+        type: 'confirm',
+        name: 'Overwrite',
+        message: 'now.json already exists! Would you like to Overwrite?',
         default: false,
       },
     ])
@@ -71,7 +95,7 @@ if (existingconfig) {
       if (answers.overwrite) {
         buildconfig();
       } else {
-        console.log("GoodBye!");
+        console.log('GoodBye!');
       }
     });
 } else {
